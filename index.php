fa49c18d96eabc75c0f876525063f7f2c0f85f46
FlorenzaJ <florenzajvn@gmail.com>
2025-03-05 01:44:10 +0700

    Commit index.php

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
-tes upload
-tes commit lagi
+<?php
+session_start();
+require_once 'config/database.php';
+require_once 'routes/web.php';
+
+$url = isset($_GET['url']) ? $_GET['url'] : 'home';
+$router = new Router();
+$router->route($url);
